6c266f8da8438d2142b34dc2e4ba464d23c075a3
Dimitar Dimitrov <dimitar.dimitrov@gmail.com>
2017-04-12 12:33:49 +0900

    Added readme and convention mappings. Updated to Gradle 3.5.

---
+++ README.md
@@ -0,0 +1,56 @@
+A Gradle plugin, uploading code coverage results to [Codacy.com](https://www.codacy.com/).
+I am creating it because [jpm4j](http://jpm4j.org/) broke recently, and I find the 
+[proposed](https://github.com/codacy/codacy-coverage-reporter)
+[workarounds](https://github.com/mountain-pass/hyperstate/commit/857ca93e1c8484c14a5e2da9f0434d3daf3328ce)
+ are clumsy and involving copy/paste of lumps of code or jitpacking something from a github repo.
+
+When/if Codacy provide official plugin, this plugin will be deprecated.
+
+# Migrating from working TravisCI setup
+Open your `build.gradle` file and add this line to the `plugins` section (you may also remove 
+`jacoco` while you are at it):
+
+```
+id "io.github.ddimtirov.codacy" version "0.1.0"
+```
+
+Then add this line after the `plugins` section:
+
+```
+repositories.maven { url "http://dl.bintray.com/typesafe/maven-releases" } // FIXME: codacy uploader needs this
+```
+
+Done!
+
+# Usage
+Assuming your environment is set up, you can use the aggregating task:
+
+```
+gradlew codacyUpload
+```
+
+If not, you need to upload each coverage result individually and specify 
+the token on the command line:
+
+```
+gradlew jacocoTestReportCodacyUpload --codacy-token fe12d3a13ce28cb7b3c2873b46
+```
+
+# Configuration
+You can specify params [only when calling the individual publishing tasks](https://discuss.gradle.org/t/options-for-a-dependent-task/22275).
+
+To find the available parameters and documentation:
+
+```
+gradlew help --task jacocoTestReportCodacyUpload
+```
+
+You can also set these from the build script through the plugin extension as follows:
+
+```
+codacy {
+    toolVersion = '1.6' // override the version for com.codacy:codacy-coverage-reporter
+    commitUuid = '2ca92c64bbcb7f97b204d3ee11ebd1e7075ea35f'
+    projectToken = 'fe12d3a13ce28cb7b3c2873b46'
+}
+```
--- src/main/groovy/io/github/ddimtirov/gradle/codacy/CodacyPlugin.groovy
+++ src/main/groovy/io/github/ddimtirov/gradle/codacy/CodacyPlugin.groovy
@@ -5,20 +5,18 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.internal.tasks.options.Option
-import org.gradle.api.tasks.Classpath
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.TaskAction
 import org.gradle.testing.jacoco.plugins.JacocoPlugin
 import org.gradle.testing.jacoco.tasks.JacocoReport
-
 // TODO: deploy descriptors, test, doc, publish
 class CodacyPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         def codacyExt = project.extensions.create('codacy', CodacyExtension)
-        def codacyClasspath = project.configurations.create('codacy') { c ->
+        project.configurations.create('codacy') { c ->
             c.defaultDependencies { dependencies ->
                 dependencies << project.dependencies.create(
                         group: 'com.codacy',
@@ -41,8 +39,9 @@ class CodacyPlugin implements Plugin<Project> {
                 project.tasks.create("${task.name}CodacyUpload", CodacyUploadTask) {
                     it.group = 'publishing'
                     it.description = "Upload $task.name to Codacy.com"
-                    it.coverageReport = { reportTask.reports.xml.destination }
-                    it.classpath codacyClasspath
+                    it.conventionMapping.coverageReport = { reportTask.reports.xml.destination }
+                    it.conventionMapping.commitUuid     = { codacyExt.commitUuid }
+                    it.conventionMapping.projectToken   = { codacyExt.projectToken }
                     it.dependsOn task
                     codacyUploadTask.dependsOn it
                 }
@@ -52,24 +51,28 @@ class CodacyPlugin implements Plugin<Project> {
 }
 
 class CodacyExtension {
-    def toolVersion = '1.0.7'
+    String toolVersion = '1.0.7'
+    String commitUuid
+    String projectToken
 }
 
 class CodacyUploadTask extends DefaultTask {
     @Option(option='commit-uuid', description = 'Commit UUID used by Codacy to track the current results. Typically inferred from the environment.')
-    @Optional @Input commitUuid
+    @Optional @Input String commitUuid
 
-    @InputFile coverageReport
-    @Classpath classpath
+    @Option(option='codacy-token', description = 'Codacy project token. Typically inferred from the environment.')
+    @Optional @Input String projectToken
+
+    @InputFile File coverageReport
 
     @TaskAction publishCoverageToCodacy() {
         project.javaexec {
             it.main = "com.codacy.CodacyCoverageReporter"
-            classpath = project.configurations.codacy
-            it.classpath classpath
+            it.classpath project.configurations.codacy
             it.args "-l", "Java"
-            it.args "-r", project.file(coverageReport)
-            if (commitUuid) it.args '--commitUUID', commitUuid
+            it.args "-r", getCoverageReport()
+            if (commitUuid) it.args '--commitUUID', getCommitUuid()
+            if (projectToken) it.args '--projectToken', getProjectToken()
         }
     }
 }
